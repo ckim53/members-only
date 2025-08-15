150fbab44eb3481ae7e0435089357c96191391bb
Christina <184446177+ckim53@users.noreply.github.com>
2025-08-15 13:27:40 -0700

    Clean up config for production

--- config/pool.js
+++ config/pool.js
@@ -2,11 +2,10 @@ require("dotenv").config();
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: "christinakim",
-  host: "localhost",
-  database: "clubhouse",
-  password: "0805",
-  port: 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = {
